aa163e075718e5102a459fe8b94de114038d7876
Zero <zeroliberationarmy@gmail.com>
2019-09-23 17:05:54 +0100

    tidy up

--- Scripts/NStoggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if xdotool search --onlyvisible --name "NetSpeed"; then
-        i3-msg [title="NetSpeed"] move scratchpad
-else
-        i3-msg [title="NetSpeed"] move position mouse, resize set 600 350
-        i3-msg [title="NetSpeed"] move down 30
-fi
