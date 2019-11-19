8dcd4203a288adb1f7583850c45c361a86e5252b
Zero <zeroliberationarmy@gmail.com>
2019-11-19 16:38:24 +0000

    fix permissions

--- Scripts/rack_update.sh
+++ Scripts/rack_update.sh
@@ -10,11 +10,12 @@ curl "$base_url""$sdk_zip" --output /mnt/Gog/Downloads/"$sdk_zip"
 
 unzip /mnt/Gog/Downloads/"$rack_zip" -d /mnt/Gog/Downloads
 unzip /mnt/Gog/Downloads/"$sdk_zip" -d /mnt/Gog/Downloads
-
+ 
 rm -r /opt/vcvrack
 rm -r /mnt/Magog/Rack-SDK
 
 mv /mnt/Gog/Downloads/Rack /opt/vcvrack
 mv /mnt/Gog/Downloads/Rack-SDK /mnt/Magog/Rack-SDK
+chmod -R 2777 /mnt/Magog/Rack-SDK
 
 setcap cap_sys_nice=ep /opt/vcvrack/Rack
