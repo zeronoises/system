23ce9b490dfeab66c78632cb510ff408509751a6
Zero <zeroliberationarmy@gmail.com>
2020-02-27 10:16:12 +0000

    buncha bunch

--- Scripts/polybar-scripts/popup-calendar.sh
+++ Scripts/polybar-scripts/popup-calendar.sh
@@ -4,7 +4,7 @@ BAR_HEIGHT=25  # polybar height
 BORDER_SIZE=2  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=168 # 193 is minimum possible value
-DATE="$(date +"%A %B %e %Y")"
+DATE="$(date +"%a %b %e")"
 
 case "$1" in
 --popup)
