3bac53425220ece6cd5e0c7e533037fcae48aeca
Zero <zeroliberationarmy@gmail.com>
2021-04-12 20:54:18 +0100

    Parked HLWM & dzen

--- HLWM-DZEN2/panels.sh
+++ HLWM-DZEN2/panels.sh
@@ -2,17 +2,17 @@
 
 monitor="$@"
 mon=$((monitor-1))
-command_in='herbstclient --idle'
+# command_in='herbstclient --idle'
 len=$(cat /mnt/Gog/updates.txt | wc -l)
 font="Hack Nerd Font:size 14"
 
 
-$command_in | while read event; do (herbstclient tag_status $mon | tr -d '\t'); done \
+# $command_in | while read event; do (herbstclient tag_status $mon | tr -d '\t'); done \
                                 | dzen2 -p -fn "$font" -xs $monitor \
                                   -ta c -x 10 -y 3 -h 26 -tw 180 -fg '#DADF67' -bg '#555555'\
                                   -title-name dzentags$monitor &
 
-$command_in | while read event; do ta=$(herbstclient attr monitors.$mon.tag); \
+# $command_in | while read event; do ta=$(herbstclient attr monitors.$mon.tag); \
                                   herbstclient attr tags.by-name.$ta.focused_client.title; done \
                                 | dzen2 -p -fn "$font" -xs $monitor \
                                   -ta c -x 200 -y 3 -h 26 -tw 560 -fg '#DADF67' -bg '#555555'\
